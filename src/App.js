0119728f872ac0d525c691f57f516960179965e7
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-03 12:50:36 +0600

    Row component declaration & style done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './App.css';
+import Row from './components/Row/Row';
+import requests from "./request";
 
 const App = () => {
     return (
         <div>
-            React APP
+            <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
         </div>
     )
 }
